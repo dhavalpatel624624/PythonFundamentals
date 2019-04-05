ffb70261e546ee89ae6f81592fad3f550777fc69
Dhaval patel <dhavalpatel624624@gmail.com>
2019-04-05 16:13:25 -0500

    Initial commit with basic Python Fundamentals

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyfund',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='pateldh',
+    author_email='',
+    description=''
+)
